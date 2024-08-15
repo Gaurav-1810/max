acf5639647744fdc8c3dae9543f8631db19e16c1
gaurav mehra <gauravmehra1810gmail.com>
2024-08-15 17:44:08 +0530

    container config done

---
+++ JAVA SCRIPT/intro.js
@@ -0,0 +1,17 @@
+//Data  type in javascript
+//numbers
+var num1 = 455;
+var num2 = 45;
+//string
+var str1 = "this is a string";
+var string = "this is also a string";
+
+objects
+let marks = {
+    ravi : 75,
+    gaurav : 88,
+    harry : 95,
+
+}
+console.log(marks);
+
